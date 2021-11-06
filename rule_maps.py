e07e271e5fb805b985a5de72d0849b6ee531e645
Wesley Silva <88410575+jwesleylima@users.noreply.github.com>
2021-11-06 19:17:14 -0300

    fontes principais adicionadas

---
+++ rule_maps.py
@@ -0,0 +1,62 @@
+"""It contains pre-made rule maps."""
+
+
+"""
+Copyright (c) 2021 JWesleyLima
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+BASIC_MAP = {
+	# Images
+	'png': 'Images',
+	'jpeg': 'Images',
+	'jpg': 'Images',
+	'jfif': 'Images',
+	'tiff': 'Images',
+	# Videos
+	'mp4': 'Videos',
+	# Musics
+	'mp3': 'Musics',
+	'wav': 'Musics',
+	'ogg': 'Musics',
+	# Gif
+	'gif': 'Gif Images',
+	# Common
+	'exe': 'Executables',
+	'msi': 'Executables',
+	'rar': 'Compressed Files',
+	'zip': 'Compressed Files',
+	'7z': 'Compressed Files',
+	'pdf': 'Documents',
+	'docx': 'Documents',
+	'pptx': 'Documents',
+	'txt': 'Text Documents',
+	# Programming
+	'py': 'Python Sources',
+	'js': 'JavaScript Sources',
+	'ts': 'TypeScript Sources',
+	'rs': 'Rust Sources',
+	'java': 'Java Sources',
+	'kt': 'Kotlin Sources',
+	'json': 'JSON Documents',
+	'php': 'PHP Documents',
+	'xml': 'XML Documents'
+}
