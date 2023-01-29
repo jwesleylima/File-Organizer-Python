0c00d6eed8e29a1c10db8dba125d316063e283c6
Wesley Silva <jwesleylima.developer@gmail.com>
2023-01-29 18:56:50 -0300

    License updated and license added to the main.py file.

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+# GitHub: https://github.com/jwesleylima
+# Instagram: https://instagram.com/jwesleylimadev
+
+# MIT License
+
+# Copyright (c) 2021-2023 JWesleyLima
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included 
+# in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 """Organizer is a class that makes it easy to 
 organize and manipulate files."""
 
@@ -8,7 +34,7 @@ from typing import (Any, Union, Callable, Iterable,
                     Dict, NoReturn)
 
 
-class Organizer(object):
+class Organizer:
     """Class that allows you to organize and manipulate files.
 
     Organizer(target_path: Union[str, PurePath, PathLike[str]], 
